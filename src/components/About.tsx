aef2a6f19fa522129367d43916e0ae5bd956f87a
Rishad010 <rdm3745@gmail.com>
2026-05-22 23:34:34 +0530

    update portfolio sections

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import profile from "../profile.jpg";
+import profile from "../profile.png";
 import { FaLaptopCode, FaCloud, FaQuinscape } from "react-icons/fa";
 import { SiCyberdefenders } from "react-icons/si";
 
